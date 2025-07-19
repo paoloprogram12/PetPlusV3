88ece821bfb8b2fe526b4a47c40eb5ef20a0af46
paoloprogram12 <sethpsalazar@gmail.com>
2025-07-18 22:04:19 -0700

    asd

--- PetPlusV2/Views/MainTabView.swift
+++ PetPlusV2/Views/MainTabView.swift
@@ -8,8 +8,58 @@
 import SwiftUI
 
 struct MainTabView: View {
+    @State private var selection: Int = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .bottom) {
+                    TabView(selection: $selection) {
+                        // TODO: Replace dashboard views with custom views
+                        Text("Food")
+                            .tabItem {
+                                Label("Food", systemImage: ("fork.knife.circle.fill"))
+                                    .foregroundColor(Color("mainColor"))
+                            }
+                            .tag(0)
+                        Text("Exercise")
+                            .tabItem {
+                                Label("Exercise", systemImage: ("figure.run.circle.fill"))
+                                    .foregroundColor(Color("mainColor"))
+                            }
+                            .tag(1)
+                        Spacer()
+                            .tabItem {
+                                EmptyView()
+                            }
+                            .tag(2)
+                        Text("Services")
+                            .tabItem {
+                                Label("Services", systemImage: ("magnifyingglass.circle.fill"))
+                                    .foregroundColor(Color("mainColor"))
+                            }
+                            .tag(3)
+                        Text("Profile")
+                            .tabItem {
+                                Label("Profile", systemImage: ("person.crop.circle.fill"))
+                                    .foregroundColor(Color("mainColor"))
+                            }
+                            .tag(4)
+                    }
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "house.circle.fill")
+                            .tint(Color(.white))
+                            .font(.title)
+                    }
+                    .frame(width: 50, height: 50)
+                    .background(Color("mainColor"))
+                    .clipShape(Circle())
+                }
+                .ignoresSafeArea(.keyboard)
+                .onChange(of: selection, perform: { [selection] newValue in
+                    if newValue == 2 {
+                        self.selection = selection
+                    }
+                })
     }
 }
 
