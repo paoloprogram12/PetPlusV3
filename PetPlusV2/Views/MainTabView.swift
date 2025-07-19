b7af44f14b843ca430c50f2401b5e45c50f8d7c3
paoloprogram12 <sethpsalazar@gmail.com>
2025-07-18 21:59:20 -0700

    import

---
+++ PetPlusV2/Views/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  PetPlusV2
+//
+//  Created by Seth Paolo Salazar on 7/18/25.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
