a51c921d8347f22ac5cb1cea72ae987a5d825595
paoloprogram12 <sethpsalazar@gmail.com>
2025-07-20 14:19:22 -0700

    import services

---
+++ PetPlusV2/Views/ServicesView.swift
@@ -0,0 +1,18 @@
+//
+//  ServicesView.swift
+//  PetPlusV2
+//
+//  Created by Seth Paolo Salazar on 7/20/25.
+//
+
+import SwiftUI
+
+struct ServicesView: View {
+    var body: some View {
+        Text("Services")
+    }
+}
+
+#Preview {
+    ServicesView()
+}
