31abdac0fb7b04697afe17322b5715a0270fda03
paoloprogram12 <sethpsalazar@gmail.com>
2025-07-20 14:18:12 -0700

    import exercise

---
+++ PetPlusV2/Views/ExerciseView.swift
@@ -0,0 +1,18 @@
+//
+//  ExerciseView.swift
+//  PetPlusV2
+//
+//  Created by Seth Paolo Salazar on 7/20/25.
+//
+
+import SwiftUI
+
+struct ExerciseView: View {
+    var body: some View {
+        Text("Exercise")
+    }
+}
+
+#Preview {
+    ExerciseView()
+}
