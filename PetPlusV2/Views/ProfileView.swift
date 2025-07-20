b8b72ba9f198db68ea0dc7947cdf3d73107e85ed
paoloprogram12 <sethpsalazar@gmail.com>
2025-07-20 14:07:40 -0700

    import profile view

---
+++ PetPlusV2/Views/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  PetPlusV2
+//
+//  Created by Seth Paolo Salazar on 7/20/25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
