aab04bf9406a9fd6ed89b0e33104d3e7c82f99f1
paoloprogram12 <sethpsalazar@gmail.com>
2025-07-20 14:06:24 -0700

    import the food view

---
+++ PetPlusV2/Views/FoodView.swift
@@ -0,0 +1,18 @@
+//
+//  FoodView.swift
+//  PetPlusV2
+//
+//  Created by Seth Paolo Salazar on 7/20/25.
+//
+
+import SwiftUI
+
+struct FoodView: View {
+    var body: some View {
+        Text("Food")
+    }
+}
+
+#Preview {
+    FoodView()
+}
